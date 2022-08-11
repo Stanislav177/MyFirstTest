d018506b2d6eb7b6e653439e188e26e16ea143b3
Stanislav <stas.177@mail.ru>
2022-08-11 16:07:46 +0300

    добавил бизнес логику ввода email

--- app/src/main/java/com/example/myfirsttest/MainActivity.kt
+++ app/src/main/java/com/example/myfirsttest/MainActivity.kt
@@ -1,11 +1,31 @@
 package com.example.myfirsttest
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.example.myfirsttest.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityMainBinding
+    private val emailValidator = EmailValidator()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        with(binding) {
+            editTextTextEmailAddress.addTextChangedListener(emailValidator)
+
+            btnSave.setOnClickListener {
+                if (emailValidator.isValid) {
+                    Toast.makeText(this@MainActivity, "OK", Toast.LENGTH_LONG).show()
+                } else {
+                    val errorEmail = getString(R.string.error_email)
+                    editTextTextEmailAddress.error = errorEmail
+                }
+            }
+        }
     }
 }
